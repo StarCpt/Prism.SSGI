f7c237f352687c254df95526937725a6d7c34f61
StarCpt <86216339+StarCpt@users.noreply.github.com>
2025-12-07 06:03:54 -0800

    revert using blurred ssgi output when history is low as it causes ghosting

--- Prism.Render/Shaders/SSGI/Denoiser/temporal.hlsl
+++ Prism.Render/Shaders/SSGI/Denoiser/temporal.hlsl
@@ -77,7 +77,7 @@ float4 ps(const float4 position : SV_Position, const float2 uv : TEXCOORD) : SV_
         // something to keep in mind when adjusting the normal diff threshold.
         float3 prevViewNormalReproj = ReprojectPrevViewNormal(UnpackNormal(prevGBuffer1[offsetPos].xy));
         
-        static const float DEPTH_DIFF_THRESHOLD = 0.1; // meters
+        static const float DEPTH_DIFF_THRESHOLD = 0.05; // meters
         static const float NORMAL_DOT_DIFF_THRESHOLD = 0.5;
         
         bool depthRejected = depthDiff > (DEPTH_DIFF_THRESHOLD * dot_ray_surface_inv);
@@ -97,7 +97,7 @@ float4 ps(const float4 position : SV_Position, const float2 uv : TEXCOORD) : SV_
     // from the reblur slides - using blurred input when history is lacking
     // better to use edge-aware upscaling but this lazy version seems good enough
     float mip = max(0, -(history.w - 1) + 4);
-    float3 currentColor = Source.SampleLevel(LinearSampler, uv, mip);
+    float3 currentColor = Source.SampleLevel(LinearSampler, uv, 0);
     
     float3 finalColor = lerp(history.xyz, currentColor, 1.0 / history.w);
     return float4(finalColor, history.w);
