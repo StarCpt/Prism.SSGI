5e175eef7d15955105554be9dad3157aac6319af
StarCpt <86216339+StarCpt@users.noreply.github.com>
2025-12-09 13:07:19 -0800

    Fix >.net core 2.0 compilation

--- Prism.Maths/MathHelpers.cs
+++ Prism.Maths/MathHelpers.cs
@@ -4,23 +4,11 @@ internal class MathHelpers
 {
     public static float Clamp(float value, float min, float max)
     {
-#if NETCOREAPP2_0_OR_GREATER
-        return Math.Clamp(value, min, max);
-#else
-        if (value < min) return min;
-        else if (value > max) return max;
-        return value;
-#endif
+        return VRageMath.MathHelper.Clamp(value, min, max);
     }
 
     public static int Clamp(int value, int min, int max)
     {
-#if NETCOREAPP2_0_OR_GREATER
-        return Math.Clamp(value, min, max);
-#else
-        if (value < min) return min;
-        else if (value > max) return max;
-        return value;
-#endif
+        return VRageMath.MathHelper.Clamp(value, min, max);
     }
 }
