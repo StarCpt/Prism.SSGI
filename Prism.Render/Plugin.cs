8d010070d80f36db9c597fb9ae5ff9d4a884d63f
StarCpt <86216339+StarCpt@users.noreply.github.com>
2025-12-12 12:58:19 -0800

    Move assembly attributes to AssemblyInfo.cs

--- Prism.Render/Plugin.cs
+++ Prism.Render/Plugin.cs
@@ -18,9 +18,6 @@ using VRage.Render11.GeometryStage2.Instancing;
 using VRage.Render11.GeometryStage2.PreparePass;
 using VRageRender;
 
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
-
 namespace Prism.Render;
 
 public class Plugin : IPlugin
--- Prism.Render/Properties/IgnoreAccessChecksToAttribute.cs
+++ Prism.Render/Properties/IgnoreAccessChecksToAttribute.cs
@@ -1,8 +1,4 @@
-﻿[assembly: System.Runtime.CompilerServices.IgnoresAccessChecksTo("VRage")]
-[assembly: System.Runtime.CompilerServices.IgnoresAccessChecksTo("VRage.Render")]
-[assembly: System.Runtime.CompilerServices.IgnoresAccessChecksTo("VRage.Render11")]
-
-namespace System.Runtime.CompilerServices
+﻿namespace System.Runtime.CompilerServices
 {
     [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
     internal sealed class IgnoresAccessChecksToAttribute : Attribute
