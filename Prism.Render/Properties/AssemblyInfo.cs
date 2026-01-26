abcdd8c94447024aa0efc62e6fe532c84655d30b
StarCpt <86216339+StarCpt@users.noreply.github.com>
2026-01-26 04:15:34 -0800

    v1.0.2 version bump

--- Prism.Render/Properties/AssemblyInfo.cs
+++ Prism.Render/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
 
 [assembly: System.Runtime.CompilerServices.IgnoresAccessChecksTo("VRage")]
 [assembly: System.Runtime.CompilerServices.IgnoresAccessChecksTo("VRage.Render")]
